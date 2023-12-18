435ff806b7c70e84edc336cde724d4f6bdef004f
Fabian Petersen <fabian@nf-petersen.de>
2023-12-18 22:46:00 +0100

    commenting game hard

--- libraries/Game/GameHard.cpp
+++ libraries/Game/GameHard.cpp
@@ -29,6 +29,7 @@ void GameHard::generate_tilt_sequence()
     for (int i = 1; i < m_tilt_sequence_size; i++)
     {
         int orientation;
+        // Generate a random orientation that is not the same as the last orientation
         do
         {
             orientation = random(0, 4);
@@ -47,11 +48,13 @@ void GameHard::generate_clock_values()
 
 void GameHard::generate_rotate_values(int leftSegment, int rightSegment)
 {
+    // Generate a random left segment that is not the current left segment
     do
     {
         m_left_rotate_segment = random(0, 8);
     } while (m_left_rotate_segment == leftSegment);
 
+    // Generate a random right segment that is not the current right segment
     do
     {
         m_right_rotate_segment = random(0, 8);
@@ -60,50 +63,57 @@ void GameHard::generate_rotate_values(int leftSegment, int rightSegment)
 
 void GameHard::play_clock_melody(int hour, int minute)
 {
-    if (this->m_sound_player->isEmpty())
+    if (!m_sound_player->isEmpty())
     {
-        int melody[25] = {};
-        int durations[25] = {};
-        for (int i = 0; i < m_hour; i++)
-        {
-            melody[i] = NOTE_C2;
-            durations[i] = 4;
-        }
-        melody[m_hour] = 0;
-        durations[m_hour] = 1;
-
-        for (int i = m_hour + 1; i < m_hour + 1 + m_minute; i++)
-        {
-            melody[i] = NOTE_C4;
-            durations[i] = 4;
-        }
-        melody[m_hour + 1 + m_minute] = 0;
-        durations[m_hour + 1 + m_minute] = 1;
-
-        this->m_sound_player->playSound(melody, durations, m_hour + 1 + m_minute + 1);
+        return;
     }
+
+    // Play as many notes as the hour is and then one note for every 5 minutes
+    int melody[25] = {};
+    int durations[25] = {};
+    for (int i = 0; i < m_hour; i++)
+    {
+        melody[i] = NOTE_C2;
+        durations[i] = 4;
+    }
+    melody[m_hour] = 0;
+    durations[m_hour] = 1;
+
+    for (int i = m_hour + 1; i < m_hour + 1 + m_minute; i++)
+    {
+        melody[i] = NOTE_C4;
+        durations[i] = 4;
+    }
+    melody[m_hour + 1 + m_minute] = 0;
+    durations[m_hour + 1 + m_minute] = 1;
+
+    m_sound_player->playSound(melody, durations, m_hour + 1 + m_minute + 1);
 }
 
 bool GameHard::check_clock_time(int hour, int minute)
 {
+    // The hour must be correct and the minute must be within the correct 5 minutes;
     return hour == m_hour && minute == m_minute;
 }
 
 bool GameHard::check_rotate(int leftSegment, int rightSegment)
 {
+    // The left segment must be the left rotate segment and the right segment must be the right rotate segment
     return leftSegment == m_left_rotate_segment && rightSegment == m_right_rotate_segment;
 }
 
 bool GameHard::check_distance(int distance)
 {
-    int touch = this->m_touch_player->getTouchInput();
-    TiltPlayer::Tilt tilt = this->m_tilt_player->getTilt();
+    int touch = m_touch_player->getTouchInput();
+    TiltPlayer::Tilt tilt = m_tilt_player->getTilt();
 
-    Serial.print("touch: ");
-    Serial.println(touch);
-
+    // The distance must be within 1 of the correct distance
     auto distance_check = distance >= m_distance - 1 && distance <= m_distance + 1;
+
+    // All toch fields must be touched
     auto touch_check = (touch & 0b1111) == 0b1111;
+
+    // The vault must be tilted in any direction
     auto tilt_check = tilt != TiltPlayer::Tilt::None;
 
     return distance_check && touch_check && tilt_check;
