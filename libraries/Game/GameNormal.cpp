61e99cc094ce994a9e9bd51822bb3221e544f322
Fabian Petersen <fabian@nf-petersen.de>
2023-12-18 22:51:51 +0100

    commenting game normal

--- libraries/Game/GameNormal.cpp
+++ libraries/Game/GameNormal.cpp
@@ -23,6 +23,7 @@ GameNormal::GameNormal(SoundPlayer *soundPlayer, DistancePlayer *distancePlayer,
 
 void GameNormal::generate_tilt_sequence()
 {
+    // Each direction should be in the sequence once in a random order
     int orientation1 = random(0, 4);
     int orientation2;
     do
@@ -56,11 +57,13 @@ void GameNormal::generate_clock_values()
 
 void GameNormal::generate_rotate_values(int leftSegment, int rightSegment)
 {
+    // Generate a random left segment that is not the current left segment
     do
     {
         m_left_rotate_segment = random(0, 8);
     } while (m_left_rotate_segment == leftSegment);
 
+    // Generate a random right segment that is not the current right segment
     do
     {
         m_right_rotate_segment = random(0, 8);
@@ -69,32 +72,36 @@ void GameNormal::generate_rotate_values(int leftSegment, int rightSegment)
 
 void GameNormal::play_clock_melody(int hour, int minute)
 {
-    if (this->m_sound_player->isEmpty())
+    if (!m_sound_player->isEmpty())
     {
-        int melody[17] = {};
-        int durations[17] = {};
-        for (int i = 0; i < m_hour; i++)
-        {
-            melody[i] = NOTE_C2;
-            durations[i] = 4;
-        }
-        melody[m_hour] = 0;
-        durations[m_hour] = 1;
-
-        for (int i = m_hour + 1; i < m_hour + 1 + m_minute; i++)
-        {
-            melody[i] = NOTE_C4;
-            durations[i] = 4;
-        }
-        melody[m_hour + 1 + m_minute] = 0;
-        durations[m_hour + 1 + m_minute] = 1;
-
-        this->m_sound_player->playSound(melody, durations, m_hour + 1 + m_minute + 1);
+        return;
     }
+
+    // Play as many notes as the hour is and then one note for every 15 minutes
+    int melody[17] = {};
+    int durations[17] = {};
+    for (int i = 0; i < m_hour; i++)
+    {
+        melody[i] = NOTE_C2;
+        durations[i] = 4;
+    }
+    melody[m_hour] = 0;
+    durations[m_hour] = 1;
+
+    for (int i = m_hour + 1; i < m_hour + 1 + m_minute; i++)
+    {
+        melody[i] = NOTE_C4;
+        durations[i] = 4;
+    }
+    melody[m_hour + 1 + m_minute] = 0;
+    durations[m_hour + 1 + m_minute] = 1;
+
+    m_sound_player->playSound(melody, durations, m_hour + 1 + m_minute + 1);
 }
 
 bool GameNormal::check_clock_time(int hour, int minute)
 {
+    // The hour must be correct and the minute must be within the the right 15 minutes (first 3 segments);
     return hour == m_hour && (minute / 3) == m_minute;
 }
 
@@ -103,10 +110,12 @@ bool GameNormal::check_rotate(int leftSegment, int rightSegment)
     bool left_correct = leftSegment == m_left_rotate_segment;
     bool right_correct = rightSegment == m_right_rotate_segment;
 
+    // If both segments are correct, return true
     if (left_correct && right_correct)
     {
         return true;
     }
+    // If left segment is correct, play the first two notes of the keypad notes
     else if (left_correct)
     {
         if (this->m_sound_player->isEmpty())
@@ -116,6 +125,7 @@ bool GameNormal::check_rotate(int leftSegment, int rightSegment)
             this->m_sound_player->playSound(melody, durations, 3);
         }
     }
+    // If right segment is correct, play the last two notes of the keypad notes
     else if (right_correct)
     {
         if (this->m_sound_player->isEmpty())
@@ -130,12 +140,12 @@ bool GameNormal::check_rotate(int leftSegment, int rightSegment)
 
 bool GameNormal::check_distance(int distance)
 {
-    int touch = this->m_touch_player->getTouchInput();
+    int touch = m_touch_player->getTouchInput();
 
-    Serial.print("touch: ");
-    Serial.println(touch);
-
+    // The distance must be within 1 of the correct distance
     auto distance_check = distance >= m_distance - 1 && distance <= m_distance + 1;
+
+    // All toch fields must be touched
     auto touch_check = (touch & 0b1111) == 0b1111;
 
     return distance_check && touch_check;
