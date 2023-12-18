461cf94f0f9a80e8641d038d2e8af4b0f57c2fcb
Fabian Petersen <fabian@nf-petersen.de>
2023-12-18 20:59:16 +0100

    Commenting the rotateplayer

--- libraries/RotatePlayer/RotatePlayer.cpp
+++ libraries/RotatePlayer/RotatePlayer.cpp
@@ -7,11 +7,15 @@ RotatePlayer::RotatePlayer(int rotate_left_pin, int rotate_right_pin)
 
 int RotatePlayer::getSegments(int rotate_step = 128)
 {
+    // Read the values
     int leftValue = analogRead(m_rotate_left_pin);
     int rightValue = analogRead(m_rotate_right_pin);
 
+    // Divide the values into segments
     int leftSegment = leftValue / rotate_step;
     int rightSegment = rightValue / rotate_step;
 
+    // Combine the segments into one value The first 4 bits are the leftSegment
+    // and the last 4 bits are the rightSegment
     return (leftSegment << 4) | rightSegment;
 }
--- libraries/RotatePlayer/RotatePlayer.h
+++ libraries/RotatePlayer/RotatePlayer.h
@@ -1,3 +1,8 @@
+/* *******************************************************
+ *  RotatePlayer.h
+ *  This library is used to return the segment of two
+ *  potentiometers connected to the Arduino.
+ ********************************************************/
 #include <Arduino.h>
 
 #pragma once
@@ -7,10 +12,11 @@ class RotatePlayer
 public:
     RotatePlayer(int rotate_left_pin, int rotate_right_pin);
 
+    // The potientiometers return a value between 0 and 1023 so
+    // we divide it into to 8 segments with the default step of 128
     int getSegments(int rotate_step = 128);
 
 private:
     int m_rotate_left_pin;
     int m_rotate_right_pin;
-    int m_bit_shift;
 };
